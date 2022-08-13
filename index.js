d56ba7e9ebe5bd8cea85d598a8473424c92ff071
Joenn <wjoenn@gmail.com>
2022-08-13 20:21:25 +0200

    added base of mobile dropdown menu

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const banner = document.querySelector(".banner");
+const navbarMobile = document.querySelector(".navbarMobile");
+const body = document.querySelector("body");
+const bars = document.querySelector(".fa-bars")
+const xMark = document.querySelector(".fa-xmark")
 
 let winWidth = window.innerWidth;
-
 window.addEventListener("resize", () =>
     {
         if(window.innerWidth !== winWidth)
@@ -12,3 +15,28 @@ window.addEventListener("resize", () =>
     })
 
 banner.style.height = window.innerHeight;
+
+const displayNone = () =>
+    {
+        if(navbarMobile.style.display !== "none")
+            {
+                navbarMobile.style.display = "none";
+                body.classList.remove("stopScrolling");
+                bars.style.display = "inline-block";
+            }
+        else
+            {
+                navbarMobile.style.display = "block";
+                body.classList.add("stopScrolling");
+                bars.style.display = "none";
+            }
+    };
+
+bars.addEventListener("click", () =>
+    {
+        displayNone();
+    });
+xMark.addEventListener("click", () =>
+    {
+        displayNone();
+    });
